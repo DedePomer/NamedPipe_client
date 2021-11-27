4cbe84eb5ff0f697158ca277258cb049372d7d9c
Данила Чирков <t343st@gmail.com>
2021-11-27 10:36:03 +0300

    Диалог

--- NamedPipe_client/NamedPipe_client.cpp
+++ NamedPipe_client/NamedPipe_client.cpp
@@ -7,48 +7,45 @@ int main()
 {
 	HANDLE hPipe;
 	DWORD dwMode = PIPE_READMODE_MESSAGE;
-	BOOL Success;
+	BOOL Success, flag = TRUE, SuccessR;
+	LPSTR massage = calloc(200, sizeof(CHAR));
+	DWORD n;
+
 	system("chcp 1251");
-	printf("Пользователь\n");
-	while (1)
-	{
-		hPipe = CreateFile(PATH_PIPE, GENERIC_READ | GENERIC_WRITE,
-			0, NULL, OPEN_EXISTING, 0, NULL);
-		Success = SetNamedPipeHandleState(hPipe, &dwMode, NULL, NULL);
-		if (Success)
-		{
-			printf("соединение есть (клиент)\n");
-			break;
-		}
-		else
-		{
-			printf("соединения с сервером нет (клиент)\n");
-		}
-		Sleep(1000);
-	}
+	printf("Пользователь\n");	
 	while (1)
 	{ 	
 		hPipe = CreateFile(PATH_PIPE, GENERIC_READ | GENERIC_WRITE,
-			0, NULL, OPEN_EXISTING, 0, NULL);
+			0, NULL, OPEN_EXISTING, 0, NULL);	
 		Success = SetNamedPipeHandleState(hPipe, &dwMode, NULL, NULL);
 		if (Success)
-		{			
-			LPSTR massage = calloc(200, sizeof(CHAR));
-			DWORD n;
-			gets(massage);
-			WriteFile(hPipe, massage, 200, &n, NULL);
+		{	
+			if (flag)
+			{
+				printf("Введите сообщение для сервера: \n");							
+				gets(massage);
+				WriteFile(hPipe, massage, 200, &n, NULL);
+				flag = FALSE;
+			}	
+			SuccessR = ReadFile(hPipe, massage, 200, &n,NULL);
+			if (SuccessR)
+			{
+				printf("\nОтвет: %s\n",massage);
+				flag = TRUE;
+				if (massage == NULL)
+				{
+					printf("\n");
+				}
+			}
 		}
 		else
 		{
 			printf("сервер отключися\n");
-			break;
 		}
-		Sleep(1000);
-		
-
-
+		Sleep(1000);		
+		CloseHandle(hPipe);
 	}
-	CloseHandle(hPipe);
+	
 }
 
 
